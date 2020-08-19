aec4f1d37afae42846f45230f8560a06b1d26916
febwinter <skyv2080@gmail.com>
2020-08-19 17:50:25 +0900

    json print function added

--- jsonPrint_Test.py
+++ jsonPrint_Test.py
@@ -1,3 +1,34 @@
+from prettytable import PrettyTable
+
+def Show_DB(re_data):
+    col = []
+    table_data = []
+    data = re_data
+    if len(data) != 0:
+        col = list(data[0].keys())
+        # print(col)
+        # print(len(data)) # 반환된 데이터 갯수 체크 --> 첫 분기를 위한 조건
+
+        # for d in data:
+        #     table_data.append(list(d.values()))
+        #     print(list(d.values()))
+
+        t = PrettyTable(col)
+        resultList = []
+        
+
+        for row in data:
+            d = list(row.values())
+            # print(d)
+            t.add_row(d)
+            resultList.append(d) 
+        print(t)
+        return resultList
+    else:
+        col = ["No Result"]
+        print(t)
+        return []
+
 #쿼리문
 # # SELECT * from Customer
 
@@ -65,6 +96,11 @@
 - 반환되는 Data값은 튜플 리스트 형태
 
 [SHOW DB 함수에 리스트 형태로 넘기기]
+- 첫번째 요소에서 column 값으로 사용할 내용을 뽑아낸다 --> Beautiful Table에서 column으로 사용할 것
+        - 각 요소는 dict 타입으로 되어있어 key값을 뽑아내 사용한다.
+- 만약 반환값이 없는 경우 --> 검색값이 0개일 경우 예외 처리 : 빈 테이블에 column을 한개로 처리하고 No Result라고 뜨게 한다.
+                    -->  ShowDB의 반환값인 resultList도 빈 리스트를 반환하도록 한다.
+
 """
 data = [{
     'seller_id': 1,
@@ -88,7 +124,7 @@ data = [{
     'store': 'Boramae'
 }, {
     'seller_id': 5,
-    #'name': 'Mathal',
+    'name': 'Mathal',
     'phone': '010-2222-4444',
     'store': 'Boramae'
 }, {
@@ -122,3 +158,23 @@ data = [{
     'phone': '010-2222-1010',
     'store': 'Boramae'
 }]
+
+# data[0] --> {
+#     'seller_id': 1,
+#     'name': 'Whang',
+#     'phone': '010-2222-0000',
+#     'store': 'Boramae'
+# }
+# table_data = []
+# column = list(data[0].keys())
+
+# print(len(data)) # 반환된 데이터 갯수 체크 --> 첫 분기를 위한 조건
+
+# for d in data:
+#     table_data.append(list(d.values()))
+
+# print(table_data)
+
+
+Show_DB(data)
+Show_DB({[]})
